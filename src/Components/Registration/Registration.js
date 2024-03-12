2b0a9e9ecb17c7abe406e1101523f3a5f1ff7b9b
Sanika goyal <sanika2110118@akgec.ac.in>
2024-03-12 15:51:39 +0530

    UPDATE: changes in Home, Committe, Registration and Call for Papers Page

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,106 +1,136 @@
 import React, { Component } from "react";
 import { Table } from "react-bootstrap";
 
-    class Theme extends Component {
-            
-        render(){
-            return (
-            <div id="registration" className="target mb-5 foothandle">
-            <div class="container">
-                <h1 className="mb-3 green-heading" style={{height:"40vh"}}>Will be Updated Soon...</h1>
-            </div>
-            {/* <div className="container" >
-            <h1 className="mb-3 green-heading">Registration Fees</h1>
-            
-            
-                <Table bordered>
+class Theme extends Component {
+  render() {
+    return (
+      <div id="registration" className="target mb-5 foothandle">
+        {/* <div class="container">
+          <h1 className="mb-3 green-heading" style={{ height: "40vh" }}>
+            Will be Updated Soon...
+          </h1>
+        </div> */}
+        <div className="container">
+          <h1 className="mb-3 green-heading">Registration Fees</h1>
+          <ul className="my-3">
+            <li>
+              At least one author of each accepted paper must register at{" "}
+              <b>Full Fee</b> before the deadline for the paper to be presented
+              in the conference and included in IEEE Xplore.
+            </li>
+            <li>
+              The manuscripts may contain six pages. Up to two (2) additional
+              pages are permissible with over-length page charges . The maximum
+              number of pages per manuscript allowed is eight (8).
+            </li>
+            <li>
+              <b>Extra pages Charges : Rs 2000 or USD 25 Per Extra Page</b>
+            </li>
+          </ul>
+          <Table bordered>
             <tbody>
-            <tr>
-                        <th>Early Birds</th>
-                        <th>Indian Author</th>
-                        <th>Foreign Author</th>
-                    </tr>
-            <tr>
-                <td> Participants from Industries </td> 
-                <td>Rs. 6000</td>
-                <td>$ 250</td>
-            </tr>
-            <tr>
-                <td> Academia </td> 
-                <td>Rs. 5000</td>
+              <tr>
+                <th>Registration Category</th>
+                <th>Registration Fees</th>
+              </tr>
+              <tr>
+                <td>Non-IEEE Professional</td>
+                <td>INR 8000</td>
+              </tr>
+              <tr>
+                <td> IEEE Professional Members</td>
+                <td>INR 7000</td>
+              </tr>
+              <tr>
+                <td> Non-IEEE Students </td>
+                <td>INR 7000</td>
+              </tr>
+              <tr>
+                <td>IEEE Student Members</td>
+                <td>INR 6000</td>
+              </tr>
+              <tr>
+                <td>Industry Participants </td>
+                <td>INR 8000</td>
+              </tr>
+              <tr>
+                <td>Non-IEEE Foreign Participants </td>
                 <td>$ 200</td>
-            </tr>
-            <tr>
-                <td> Research Scholars and students </td> 
-                <td>Rs. 4000</td>
+              </tr>
+              <tr>
+                <td>IEEE Foreign Participants </td>
                 <td>$ 150</td>
-            </tr>
-            <tr>
-                <td>Poster Presentation </td> 
-                <td>Rs. 1500</td>
-                <td>$ 150</td>
-            </tr>
-            <tr>
-                <td>Co-authors & Attendees  </td> 
-                <td>Rs. 1500</td>
-                <td>$ 150</td>
-            </tr>
-
+              </tr>
             </tbody>
-        </Table>
+          </Table>
 
-<p>10% fee concession to IEI and IEEE Members.  The fee entitles the delegates to participate in technical sessions, conference kit, copy of the proceedings, lunch and refreshments on each day of conference. Registration and rest proceedings will be done through the conference website: akgec2022@akgec.ac.in</p>
-            
-            <h1 className="green-heading">Fee Submission Details</h1>
+          <p>
+            <b>Full registration includes: </b>
+            Access to plenary sessions (on prior booking only), all the
+            conference sessions, conference registration kit, lunches, tea/
+            coffee breaks, and inclusion of presented papers in IEEE Xplore.
+          </p>
 
-            <Table bordered>
+          <p>
+            <b>Student registration includes: </b>
+            Access to plenary sessions (on prior booking only), all the
+            conference sessions, conference registration kit, lunches , tea/
+            coffee breaks, and inclusion of presented papers in IEEE Xplore.
+            Students must provide valid proof (copy of valid student ID card or
+            letter from their institution or program director) by e-mail when
+            submitting their registration and payment. Registration will not be
+            effective until the validity of this document is confirmed.
+          </p>
+          <h1 className="green-heading">Payment Details</h1>
+
+          <Table bordered>
             <thead>
-                <tr>
-                    <th colSpan={2}>Details of IMPS/UPI/RTGS/NEFT:</th>
-                </tr>
+              <tr>
+                <th colSpan={2}>Details of IMPS/UPI/RTGS/NEFT:</th>
+              </tr>
             </thead>
             <tbody>
-            <tr>
+              <tr>
                 <td>Name of Beneficiary</td>
                 <td>Ajay Kumar Garg Engineering College</td>
-            </tr>
-            <tr>
-                <td> Name of the Bank </td> 
+              </tr>
+              <tr>
+                <td> Name of the Bank </td>
                 <td>Kotak Mahindra Bank Ltd.</td>
-            </tr>
-            <tr>
-                <td>Branch Address</td> 
-                <td>30 & 31 Navyug Market, P.B. No. 75, Ghaziabad-201001 (U.P.) - INDIA </td>
-            </tr>
-            <tr>
-                <td>Branch Code </td> 
+              </tr>
+              <tr>
+                <td>Branch Address</td>
+                <td>
+                  30 & 31 Navyug Market, P.B. No. 75, Ghaziabad-201001 (U.P.) -
+                  INDIA{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>Branch Code </td>
                 <td>5295</td>
-            </tr>
-            <tr>
-                <td>Bank Account No.</td> 
+              </tr>
+              <tr>
+                <td>Bank Account No.</td>
                 <td>508010250461</td>
-            </tr>
-            <tr>
-                <td>Type of Bank Account</td> 
+              </tr>
+              <tr>
+                <td>Type of Bank Account</td>
                 <td>Savings Bank Accounts</td>
-            </tr>
-            <tr>
-                <td>RTGS/NEFT/IFSC Code</td> 
+              </tr>
+              <tr>
+                <td>RTGS/NEFT/IFSC Code</td>
                 <td>KKBK0005295</td>
-            </tr>
-            <tr>
-                <td>Contact No.</td> 
+              </tr>
+              <tr>
+                <td>Contact No.</td>
                 <td>0120-2790969</td>
-            </tr>
-
+              </tr>
             </tbody>
-        </Table>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+}
 
-            </div> */}
-            </div>
-            );
-        }          
-    }
-                
-              
 export default Theme;
